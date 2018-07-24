1d2adef67772edfe7d7fbd77e151e7cbe1b990dc
Luca <urban97-luca@hotmail.it>
2018-07-24 16:12:22 +0200

    -0.28 all'ora X

--- app/src/main/java/it/unisa/tirocinio/gazzaladra/QuizMaker.java
+++ app/src/main/java/it/unisa/tirocinio/gazzaladra/QuizMaker.java
@@ -7,6 +7,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import it.unisa.tirocinio.gazzaladra.activity.fragment.quiz.Quiz1Fragment;
+import it.unisa.tirocinio.gazzaladra.activity.fragment.quiz.Quiz2Fragment;
+import it.unisa.tirocinio.gazzaladra.activity.fragment.quiz.Quiz3Fragment;
+import it.unisa.tirocinio.gazzaladra.activity.fragment.quiz.Quiz4Fragment;
+import it.unisa.tirocinio.gazzaladra.activity.fragment.quiz.Quiz5Fragment;
 import it.unisa.tirocinio.gazzaladra.activity.fragment.quiz.Quiz6Fragment;
 
 public class QuizMaker {
@@ -16,11 +21,11 @@ public class QuizMaker {
 
 	static {
 		quizList = new ArrayList<>();
-//		quizList.add(Quiz1Fragment.class.getName());
-//		quizList.add(Quiz2Fragment.class.getName());
-//		quizList.add(Quiz3Fragment.class.getName());
-//		quizList.add(Quiz4Fragment.class.getName());
-//		quizList.add(Quiz5Fragment.class.getName());
+		quizList.add(Quiz1Fragment.class.getName());
+		quizList.add(Quiz2Fragment.class.getName());
+		quizList.add(Quiz3Fragment.class.getName());
+		quizList.add(Quiz4Fragment.class.getName());
+		quizList.add(Quiz5Fragment.class.getName());
 		quizList.add(Quiz6Fragment.class.getName());
 
 		scenariList = new ArrayList<>();
